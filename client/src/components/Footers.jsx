73df66702c74a88ed162a20561a79ec4290516ab
Nabukenya Rehemah <remmiehayaan@gmail.com>
2024-06-11 12:28:50 +0300

    footer and header

--- client/src/components/Footers.jsx
+++ client/src/components/Footers.jsx
@@ -9,11 +9,11 @@ function Footers() {
    <div className='w-full max-w-7xl mx-auto'>
     <div className='grid w-full justify-between sm:flex md:grid-cols-1'>
       <div className='mt-5'>
-      <Link to="/" className='self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white'>
-    <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
-        Remy's 
+      <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
+  <span className='px-2 py-1 bg-gradient-to-r from-green-800 via-green-500 to-green-300 rounded-lg text-white'>
+        FarmVet
         </span>  
-        Blog
+        Connect
     </Link>
       </div>
 <div className='grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6'>
