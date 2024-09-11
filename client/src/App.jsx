371fc43eb394136e6792547e766ba10edd465b1f
Nabukenya Rehemah <remmiehayaan@gmail.com>
2024-09-11 13:26:47 +0300

    header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,16 +5,20 @@ import About from './pages/About'
 import Signin from './pages/Signin'
 import Signup from './pages/Signup'
 import Home from './pages/Home'
+import Header from './components/Header'
+import Projects from './pages/Projects'
 
 function App() {
   return (
    <BrowserRouter>
+   <Header/>
    <Routes>
     <Route path='/' element={<Home/>} />
     <Route path='/dashboard' element={<Dashboard/>} />
     <Route path='/about' element={<About/>} />
     <Route path='/signin' element={<Signin/>} />
     <Route path='/signup' element={<Signup/>} />
+    <Route path='/projects' element={<Projects/>}/>
     </Routes>
     </BrowserRouter>
   )
