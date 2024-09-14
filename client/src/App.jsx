edf3592f6b844d8165f0f041e2b03abc7a7964d2
Nabukenya Rehemah <remmiehayaan@gmail.com>
2024-09-14 16:16:05 +0300

    create signin api route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Signup from './pages/Signup'
 import Home from './pages/Home'
 import Header from './components/Header'
 import Projects from './pages/Projects'
+import FooterCom from './components/Footer'
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
     <Route path='/signup' element={<Signup/>} />
     <Route path='/projects' element={<Projects/>}/>
     </Routes>
+    <FooterCom/>
     </BrowserRouter>
   )
 }
