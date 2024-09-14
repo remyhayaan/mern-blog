e63557038940cc8b84c07aff51780233494408ef
Nabukenya Rehemah <remmiehayaan@gmail.com>
2024-09-14 14:58:30 +0300

    creating sign up and its functionality

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
-import router from './routes/user.route.js';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js'
 
 dotenv.config();
 
@@ -15,8 +16,21 @@ mongoose.connect(process.env.MONGO).then(
 
 const app = express();
 
+app.use(express.json());
+
 app.listen (3000, ()=>{
 console.log('server running on port 3000');
 });
 
-app.use('/api/user', router);
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+
+app.use((err, req, res , next) =>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal server error';
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
+})
