101dd0bf136f59130825eea221c46577cce63a33
Nabukenya Rehemah <remmiehayaan@gmail.com>
2024-10-13 18:31:40 +0300

    complete user image upload functionality

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -15,11 +15,11 @@
       }
     }, [location.search]);
    return (
-     <Sidebar className='w-full md: w-56'>
+     <Sidebar className='w-full md:w-56'>
       <Sidebar.Items>
         <Sidebar.ItemGroup>
             <Link to='/dashboard?tab=profile'>
-<Sidebar.Item active ={ tab=== 'profile'} icon={HiUser} label={'User'} labelColor='dark'>
+<Sidebar.Item active ={ tab=== 'profile'} icon={HiUser} label={'User'} labelColor='dark' as='button'>
 Profile
 </Sidebar.Item></Link>
 <Sidebar.Item  icon={HiArrowSmRight} className='cursor-pointer'>
